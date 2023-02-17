db0cf31e83a22f15f26c729b8b053f662605892a
NguyenManhTuanB1910326 <116878988+NguyenManhTuanB1910326@users.noreply.github.com>
2023-02-17 21:55:36 +0700

    Update contact.route.js

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -4,16 +4,16 @@ const contacts = require("../controllers/contact.controller");
 const router = express.Router();
 
 router.route("/")
-.get(contacts.findALL)
-.post(contacts.create)
-.delete(contacts.deleteALL);
+    .get(contacts.findALL)
+    .post(contacts.create)
+    .delete(contacts.deleteALL);
 
 router.route("/favorite")
-.get(contacts.findALLFavorite);
+    .get(contacts.findALLFavorite);
 
 router.route("/:id")
-.get(contacts.findOne)
-.put(contacts.update)
-.delete(contacts.delete);
+    .get(contacts.findOne)
+    .put(contacts.update)
+    .delete(contacts.delete);
 
 module.exports = router;
