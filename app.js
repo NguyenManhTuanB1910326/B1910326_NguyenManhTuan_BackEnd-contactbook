231b6cb27bcf322d0cfc09d3664377c2df948011
NguyenManhTuanB1910326 <116878988+NguyenManhTuanB1910326@users.noreply.github.com>
2023-02-17 21:55:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts",contactsRouter);
-app.use("/api/contacts",contactsRouter);
+app.use("/api/contacts", contactsRouter);
+
 
 app.get("/",(req, res) => {
     res.json({message: "welcome to contact book application."});
